68ef3eaa0b1fb8c2e15dae3463aa118f6f67fcb6
hilalkr <hilalkara0020@gmail.com>
2025-11-27 17:14:51 +0300

    feat: Implement product list interface using Jinja2 and Bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import xmlrpc.client
 import os
 from dotenv import load_dotenv
@@ -12,6 +13,8 @@ PASSWORD = os.getenv("PASSWORD")
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 def get_odoo_connection():
     common = xmlrpc.client.ServerProxy(f'{URL}/xmlrpc/2/common')
     uid = common.authenticate(DB, USER, PASSWORD, {})
@@ -23,6 +26,27 @@ def get_odoo_connection():
     
     return models, uid
 
+@app.get("/") 
+def dashboard(request: Request):
+    try:
+        models, uid = get_odoo_connection()
+        products = models.execute_kw(
+            DB, uid, PASSWORD,
+            'product.product', 'search_read', [[]],
+            {
+                'fields': ['name', 'standard_price', 'default_code', 'id'], 
+                'limit': 20
+            }
+        )
+        
+        return templates.TemplateResponse("index.html", {
+            "request": request, 
+            "products": products
+        })
+
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+    
 @app.get("/products")
 def fetch_products():
     try:

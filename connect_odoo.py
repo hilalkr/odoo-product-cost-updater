c9458b514f0a699a39fe41bf9eaa62ea5d7b9c85
hilalkr <hilalkara0020@gmail.com>
2025-11-27 15:40:41 +0300

    feat: Implemented product fetching logic using search_read

--- connect_odoo.py
+++ connect_odoo.py
@@ -17,5 +17,21 @@ uid = common.authenticate(db, username, password, {})
 if uid:
     print("Authentication successful.")
     print(f"Authenticated User ID: {uid}")
+
+    models = xmlrpc_client.ServerProxy(f"{url}/xmlrpc/2/object")
+    products = models.execute_kw(
+        db, uid, password,
+        'product.product', 
+        'search_read',      
+        [[]],               
+        {
+            'fields': ['name', 'standard_price', 'id'],
+            'limit' :5
+        }
+    )
+
+    print('Fetched products from Odoo:')
+    for product in products:
+        print(f"ID: {product['id']} | Name: {product['name']} | Cost: {product['standard_price']}")
 else:
     print("Authentication failed. Please check your credentials or database name.")
